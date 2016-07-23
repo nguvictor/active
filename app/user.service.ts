a039a8e7cb17aabc417405229503c2debbbfdab9
Victor <victor.nguyen@hotmail.com>
2016-07-23 18:50:00 -0400

    Added users service

---
+++ app/user.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+
+import { User } from './user';
+
+
+const USERS: Users[] = [
+  { id: 11, name: 'Mr. Nice' },
+  { id: 12, name: 'Narco' },
+  { id: 13, name: 'Bombasto' },
+  { id: 14, name: 'Celeritas' },
+  { id: 15, name: 'Magneta' },
+  { id: 16, name: 'RubberMan' },
+  { id: 17, name: 'Dynama' },
+  { id: 18, name: 'Dr IQ' },
+  { id: 19, name: 'Magma' },
+  { id: 20, name: 'Tornado' }
+];
+
+
+@Injectable()
+export class UserService implements OnInit {
+    getUsers(){
+        return Promise.resolve(USERS);
+    }
+}
