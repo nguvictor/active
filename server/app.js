124a9fa3cb18b8ef7cf0566915234baeaa7dcfc9
Victor <victor.nguyen@hotmail.com>
2016-07-23 22:28:20 -0400

    Working basic authentication

--- server/app.js
+++ server/app.js
@@ -2,11 +2,13 @@ var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
+var session = require('express-session');
+var passport = require('passport');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var api = require('./routes/api');
-//var authenticate = require('./routes/users');
+var authenticate = require('./routes/authenticate')(passport);
 
 var app = express();
 
@@ -17,13 +19,25 @@ app.set('view engine', 'ejs');
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
+app.use(session({
+    secret: 'ZATzXdpdwjxg94Mafr5iWyUSvqGWKkqxRa1EDPQKxcBA5u63h2x5B38olGcb',
+    resave: false,
+    saveUninitialized: true
+}));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(passport.initialize());
+app.use(passport.session());
 
+//Init passport
+var initPassport = require('./passport-init');
+initPassport(passport);
+
+app.use('/auth', authenticate);
 app.use('/api', api);
-//app.use('/auth', authenticate);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- server/routes/api.js
+++ server/routes/api.js
@@ -9,4 +9,15 @@ router.route('/messages')
         res.send({'messages':'Create Message'});
     });
 
+router.route('/messages/:id')
+    .get(function(req,res){
+        res.send({message: 'Return message with id ' + req.params.id})
+    })
+    .put(function(req, res){
+        res.send({'messages':'Modify Message with id ' + req.params.id});
+    })
+    .delete(function(req, res){
+        res.send({'messages':'Delete Message with id ' + req.params.id});
+    });
+
 module.exports = router;
